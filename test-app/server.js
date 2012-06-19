355ce7d0e0733b589530528475a9b1417f2e86ec
Tyke Lewis <tykelewis@gmail.com>
2012-06-19 14:08:33 -0700

    added better configuration and response for test app

--- test-app/server.js
+++ test-app/server.js
@@ -1,22 +1,58 @@
 var express = require( 'express' )
   , server  = express.createServer()
-  , http    = require( 'https' )
   , query   = require( 'querystring' )
   , _       = require( 'underscore' )
+  , economy = process.env.NODE_ECONOMY
+  , env     = process.env.NODE_ENV
+  
+if( _.indexOf(['real','sandbox'], economy) == -1 ) {
+    console.log( 'Invalid economy: ' + economy )
+    process.exit()
+}
+if( _.indexOf(['production','development'], env) == -1 ) {
+    console.log( 'Invalid env: ' + env )
+    process.exit()
+}
 
-var economy       = 'sandbox'
-  , client_id     = 'j4lAcOwsZ8Wdh6DObWxaYzg2sHfppF6t'
-  , client_secret = 'C62RUr2nTjAjdlCxowxCAr8VzMqdWSlp'
-  , game_id       = 'GaAIytlJEfvjJY15JbSueP'
-  , authorize_url = 'https://betable.com/authorize'
+var game_id       = 'U7unzZjEeWP9P2UySF9clY'
+  , client        = {
+        real: {
+            id     : 'JkzU4zPFJicOSx36Eb7q7FW2hkOCS50m'
+          , secret : 'TJKKyCUuPcKsAPbfYYPtCW3fGrlt68ZC'
+        }
+      , sandbox: {
+            id     : 'UkAUNkHNgSunAy2f5BzfuyoUMJXGMcDd'
+          , secret : 'RTYrlVKLIN4FsQu51dRaywHK2L8GEJWV'
+        }
+    }
+  , server_conf = {
+        production: {
+            authorize_url : 'https://betable.com/authorize'
+          , betable_host  : 'api.betable.com'
+          , betable_port  : 443
+          , path_preface  : '/1.0/'
+          , http          : require('https')
+        }
+      , development: {
+            authorize_url: 'http://players.dev.betable.com:8081/authorize'
+          , betable_host  : '127.0.0.1'
+          , betable_port  : 8020
+          , path_preface  : '/'
+          , http          : require('http')
+        }
+    }
+  , client_id     = client[economy].id
+  , client_secret = client[economy].secret
   , callback_url  = 'http://127.0.0.1:8000/callback'
-  , betable_host  = 'api.betable.com'
-  , betable_port  = 443
+  , authorize_url = server_conf[env].authorize_url
+  , betable_host  = server_conf[env].betable_host
+  , betable_port  = server_conf[env].betable_port
+  , http          = server_conf[env].http
+  , path_preface  = server_conf[env].path_preface
   , configuration = {
-        symbols:["smurf","fart","potato","zebra","billboard","goat"]
-      , window:[3,3]
-      , reels:[["zebra","smurf","fart","zebra","billboard","zebra","potato","smurf","potato","smurf","potato","potato","zebra","smurf","zebra","goat","potato","zebra","zebra","potato","billboard","potato","goat","potato"],["fart","goat","billboard","goat","goat","goat","potato","goat","smurf","zebra","fart","zebra","goat","fart","goat","billboard","smurf","smurf","billboard","billboard","smurf","goat","goat","potato"],["billboard","zebra","goat","fart","goat","smurf","goat","fart","smurf","smurf","zebra","zebra","potato","smurf","goat","zebra","potato","goat","goat","goat","goat","zebra","goat","potato"]]
-      , paylines:[[0,0,0],[1,1,1],[2,2,2],[0,1,2],[2,1,0]]
+        symbols  :["Bar","Cherry","Bell","7"]
+      , window   :[3,3]
+      , paylines :[[1,1,1],[0,0,0],[2,2,2],[0,1,2],[2,1,0]]
     }
 
 server.use( express.bodyParser() )
@@ -40,28 +76,26 @@ server.register( '.html', {
 server.use( function( req, res, next ) {
 	req.session = JSON.parse(req.cookies[ 'sid' ] || '{}' )
 	
-	res.on('header', function(){
-		res.cookie( 'sid', JSON.stringify(req.session) ) 
-	})
+	res.on('header', function() { res.cookie( 'sid', JSON.stringify(req.session) ) })
 	next()
 })
 
 var httpme = function( request, callback ) {
     var r = http.request( request )
+    if( request.body ) r.write( request.body )
 	r.on('response', function( result ) {
         var buf = ''
         result.setEncoding('utf8');
         result.on('data', function(chunk){ buf += chunk })
         result.on('end', function(){
             var data = buf
-            try {
-                data = JSON.parse( buf )
-            } catch( e ) {}
+            try { data = JSON.parse( buf ) } catch( e ) {}
             result.body = data;
-            
             callback( result )
         })
-    }).end()
+    })
+    r.on('error', function( error ) { console.log('Connection error', error, request ) })
+    r.end()
 }
 
 
@@ -71,11 +105,10 @@ server.get( '/user', function( req, res, next ) {
 		return res.redirect( authorize_url + '?client_id='+client_id+'&redirect_uri=' + escape( callback_url ) + '&response_type=code&state=' + req.session.state )
 	}	
 	
-	// Get the user object from betable API and display it
 	httpme({
 	   host   : betable_host, 
 	   port   : betable_port, 
-	   path   : '/1.0/account?access_token='+req.session.access_token,
+	   path   : path_preface + 'account?access_token='+req.session.access_token,
 	   method : 'GET'
     }, function( result_account ) {
         if( result_account.statusCode != 200 ) return res.send( result_account.body, result_account.statusCode )
@@ -83,32 +116,20 @@ server.get( '/user', function( req, res, next ) {
         httpme({
             host: betable_host
           , port: betable_port
-          , path: '/1.0/account/wallet?access_token='+req.session.access_token
+          , path: path_preface + 'account/wallet?access_token='+req.session.access_token
           , method: 'GET'
         }, function( result_wallet ) {
-            var body = {
-                account: result_account.body
-              , wallet : result_wallet.body
-            }
-            
             return res.render( 'game.html', { 
-                first_name: body.account.first_name
-              , balance   : body.wallet[economy].balance
-              , configuration: configuration
+                first_name    : result_account.body.first_name
+              , balance       : result_wallet.body[economy].balance
+              , configuration : configuration
             })
         })
     })
 		
 })
 
-server.post( '/bet', function( req, res, next ) {
-    
-    /*var payl = []
-    if( typeof req.body.paylines != 'object' ) req.body.paylines = [ req.body.paylines ]
-    for( p in req.body.paylines ) {
-        payl.push( JSON.parse( req.body.paylines[p] ) )
-    }*/
-    
+server.post( '/bet', function( req, res, next ) {    
     _.each( req.body.paylines, function( payline, index ) {
         req.body.paylines[ index ] = []
         _.each( payline, function( p ) {
@@ -117,57 +138,31 @@ server.post( '/bet', function( req, res, next ) {
     })
     
     var body = {
-       wager: req.body.wager
-     , currency: 'GBP'
-     , economy: economy
-     , location: {
-           latitude: 0
-         , longitude: 0
-       }
-     , paylines: req.body.paylines
+       wager    : req.body.wager
+     , currency : 'GBP'
+     , economy  : economy
+     , paylines : req.body.paylines
     }
     
-    var r = http.request({
+    httpme({
 	   host   : betable_host 
 	 , port   : betable_port
-	 , path   : '/1.0/game/'+game_id+'/bet?access_token='+req.session.access_token
+	 , path   : path_preface + 'games/' + game_id + '/bet?access_token=' + req.session.access_token
 	 , method : 'POST'
      , headers: {
-	       'content-type': 'application/json; charset=UTF-8'
-	     , 'content-length': JSON.stringify(body).length
+	       'content-type'   : 'application/json; charset=UTF-8'
+	     , 'content-length' : JSON.stringify(body).length
 	   }
+     , body: JSON.stringify(body)
+    }, function( response ) {
+        return res.send({
+            bet_response: response.body
+          , bet_body    : body  
+        }, response.statusCode )
     })
-    
-    r.write( JSON.stringify( body ) )
-	r.on('response', function( response ) {
-        var buf = ''
-        response.setEncoding('utf8');
-        response.on('data', function(chunk){ buf += chunk })
-        response.on('end', function(){
-            var data = buf
-            try {
-                data = JSON.parse( buf )
-            } catch( e ) {}
-            //console.log( data, response.statusCode )
-            response.body = data;
-            
-            if( response.statusCode != 201 ) {
-                return res.send( response.body, response.statusCode )
-            }
-            
-            //console.log( JSON.stringify( response.body ))
-            
-            return res.send( response.body )
-            
-            res.redirect( '/user?outcome=' + JSON.stringify(response.body.window) + '&payout=' + response.body.payout + '&paylines=' + JSON.stringify(req.body.paylines))
-        })
-    }).end()
 })
 
 server.get( '/callback', function( req, res, next ) {
-	// Got callback. User has authorized me. Exchange
-    // authorization token for access token, put it
-	// in the session, and redirect to /user.
 	var code  = req.query.code
 	  , state = req.query.state
 	  
@@ -176,56 +171,30 @@ server.get( '/callback', function( req, res, next ) {
         return res.send( 400 )
     }
     delete req.session.state
-    if( req.query.error ) {
-        return res.send( 'we got an error: ' + req.query.error )
-    }
-console.log('party')
-    console.log({
-	   host:betable_host
-     , port:betable_port
-     , path: '/1.0/token'
-     , method: 'POST'
-     , headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		authorization: 'Basic ' + new Buffer( client_id + ':' + client_secret ).toString('base64')
-	}})
-	var body = query.stringify({
-		grant_type: 'authorization_code',
-		code: code,
-		redirect_uri: 'http://127.0.0.1:8000/callback'
-	})
-	var r = http.request({
-	   host:betable_host
-     , port:betable_port
-     , path: '/1.0/token'
-     , method: 'POST'
-     , headers: {
-		'content-type': 'application/x-www-form-urlencoded'
-      , authorization: 'Basic ' + new Buffer( client_id + ':' + client_secret ).toString('base64')
-      , 'Content-Length': body.length
-	}})
-	r.on('error', function(err) {
-    console.log('unable to connect to ' + err);
-});
-	console.log('party2')
-	//console.log( 'about to write data' )
-	r.write( body )
-	console.log( 'about to response' )
-	r.on('response', function( result ) {
-	    //return console.log('got response')
-	    var buf = ''
-        result.setEncoding('utf8');
-        result.on('data', function(chunk){ buf += chunk })
-        result.on('end', function(){
-            result.body = buf;
-            if( result.statusCode != 200 ) return res.send( "result is " + result.body )
-    		req.session.access_token = JSON.parse( result.body ).access_token
-    		return res.redirect( '/user' )
-        })
-	})
-	console.log( 'about to end' )
-	r.end()
+    if( req.query.error ) return res.send( 'we got an error: ' + req.query.error )
 
+	var body = query.stringify({
+		grant_type   : 'authorization_code',
+		code         : code,
+		redirect_uri : callback_url
+	})
+	httpme({
+	   host   : betable_host 
+	 , port   : betable_port
+	 , path   : path_preface + 'token'
+	 , method : 'POST'
+	 , headers: {
+	       'content-type': 'application/x-www-form-urlencoded'
+         , authorization: 'Basic ' + new Buffer( client_id + ':' + client_secret ).toString('base64')
+         , 'Content-Length': body.length
+	   }
+     , body: body
+	   
+    }, function( result ) {
+        if( result.statusCode != 200 ) return res.send( "result is " + result.body )
+        req.session.access_token = result.body.access_token
+		return res.redirect( '/user' )
+    })
 })	
 
 server.listen( 8000 )
